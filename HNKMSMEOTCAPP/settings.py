dc8bafaffb17e10fc856f397422e89fcc6bc5043
Temesgen T Zenebe <temf2006@gmail.com>
2023-05-11 01:21:24 -0700

    start using postgresql

--- HNKMSMEOTCAPP/settings.py
+++ HNKMSMEOTCAPP/settings.py
@@ -71,14 +71,24 @@ WSGI_APPLICATION = 'HNKMSMEOTCAPP.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}"""
 
+DATABASES = {
+    'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'HNKMSM-EOTC-LV',
+    'USER': 'postgres',
+    'PASSWORD': '1117t',
+    'HOST': 'localhost',
+    'PORT': 5432
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
