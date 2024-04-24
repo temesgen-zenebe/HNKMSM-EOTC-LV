5fbbad4ec38fd8aaa5380e055b4d15b959d2b943
Temesgen T Zenebe <temf2006@gmail.com>
2024-04-24 00:33:36 -0700

    updating sermon gallery completed

--- services/views.py
+++ services/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views.generic import ListView, DetailView
-from .models import SermonMedia
+from .models import Sermon, SermonCategory, SermonMedia
 
 class ServicesView(TemplateView):
     template_name = 'services/servicesView.html'
@@ -9,12 +9,26 @@ class ServicesView(TemplateView):
 class SermonServicesView(TemplateView):
     template_name = 'services/sermon.html'
     
-
 class SermonMediaListView(ListView):
     model = SermonMedia
     template_name = 'services/sermon_media_list.html'  # Template name to be created
     context_object_name = 'sermon_media_list'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        sermon_media = SermonMedia.objects.all() 
+        context['sermon_categories'] = {}
 
+        for media in sermon_media:
+            category = media.sermon.category.category
+            if category not in context['sermon_categories']:
+                context['sermon_categories'][category] = SermonCategory.objects.filter(category=category)
+
+        return context
+
+        
+            
+            
 class SermonMediaDetailView(DetailView):
     model = SermonMedia
     template_name = 'services/sermon_media_detail.html'  # Template name to be created
